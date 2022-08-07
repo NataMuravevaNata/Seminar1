721c5fb1d36b98ae7f04182855e1a2940351a118
Nata <muravevantsh@mail.ru>
2022-08-08 00:32:59 +0300

    Example011

---
+++ Example011/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите заданное число");
+int n = int.Parse(Console.ReadLine());
+if (n<99)
+{
+   Console.WriteLine("В заданом числе нет третей цифры");
+}
+if (n<999)
+{
+    int m = (n % 10);
+    Console.WriteLine("Третья цифра числа равена:");
+    Console.WriteLine (m);
+}    
+   
