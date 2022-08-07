1bfe125be690f0fe6f4e56e38800a915d885afb6
Nata <muravevantsh@mail.ru>
2022-08-07 23:12:38 +0300

    Example007

---
+++ Example007/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите трехзначное число");
+int n = int.Parse(Console.ReadLine());
+int a = (n % 100);
+Console.WriteLine( a );
+Console.WriteLine("Вторая цифра введенного трехзначного числа:");
+int b = (a / 10);
+Console.Write( b );
