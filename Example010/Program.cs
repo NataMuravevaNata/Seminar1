d1d9fb9a629ee5b3c947e7f00cc62b846728656c
Nata <muravevantsh@mail.ru>
2022-08-08 00:07:37 +0300

    Example010

---
+++ Example010/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите заданное число");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Проверяем кратно ли второе число заданному");
+if (n%m==0)
+{
+   Console.WriteLine("Да, кратно"); 
+}
+else
+{
+    Console.WriteLine("Нет, не кратно");
+    int y = (n % m);
+    Console.WriteLine("Остаток от деления равен:");
+    Console.WriteLine (y);
+}
