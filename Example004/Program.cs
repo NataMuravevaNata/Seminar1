03008267ac98b9ded1e73f152fc95dfefc765439
Nata <muravevantsh@mail.ru>
2022-08-07 22:44:09 +0300

    Example005

--- Example004/Program.cs
+++ Example004/Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число");
+int x = int.Parse (Console.ReadLine() );
+if (x%2==0)
+{
+    Console.WriteLine("число четное");
+}
+  else
+    Console.WriteLine("число нечетное");
+ 
---
+++ Example005/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Вставить случайное число n, ");
+int n = int.Parse(Console.ReadLine ());
+int a = (-n-1);
+while (a<n-1)
+{
+        Console.Write( a );
+        Console.Write(",");
+    a=a+1;
+}
+Console.Write( n );
